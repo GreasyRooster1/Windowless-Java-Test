efae4a3ec3850a5bb3010b56597647a3d29a7a60
QuantumDeity <greasyRooster1@gmail.com>
2023-09-16 21:07:35 -0700

    Changed to render an image instead of text

--- src/main/java/QuantumDeity/WindowlessGraphics/ShapedWindowDemo.java
+++ src/main/java/QuantumDeity/WindowlessGraphics/ShapedWindowDemo.java
@@ -4,6 +4,8 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.awt.geom.Ellipse2D;
+import java.io.File;
+
 import static java.awt.GraphicsDevice.WindowTranslucency.*;
 
 public class ShapedWindowDemo extends JFrame {
@@ -16,10 +18,14 @@ public class ShapedWindowDemo extends JFrame {
         setUndecorated(true);
         setBackground(new Color(0,0,0,0)); // this is the critical line - that fourth 0 represents alpha (or opacity)
 
-        setAlwaysOnTop( true );  // keeps it in the foreground so you don't click away from it - note that clicks on the transparent part DO pass through to the desktop, at least on Lion
+        setAlwaysOnTop( true );  // keeps it in the foreground, so you don't click away from it - note that clicks on the transparent part DO pass through to the desktop, at least on Lion
 
-        JLabel testLabel = new JLabel("Floating text hah");
-        this.add(testLabel);
+        String filePath = new File("").getAbsolutePath();
+        System.out.println(filePath);
+        filePath = filePath.concat("\\src\\main\\java\\QuantumDeity\\WindowlessGraphics\\assets\\Axolotl_Base.png");
+        System.out.println(filePath);
+        JLabel imageLabel = new JLabel(new ImageIcon(filePath));
+        this.add(imageLabel);
 
     }
     public static void main(String[] args){
