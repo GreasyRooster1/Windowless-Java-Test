33890bc7c5fc9e3f4e720ae93fb63ef69e651ca2
QuantumDeity <greasyRooster1@gmail.com>
2023-09-16 17:23:15 -0700

    Changed code to render some text

--- src/main/java/QuantumDeity/WindowlessGraphics/ShapedWindowDemo.java
+++ src/main/java/QuantumDeity/WindowlessGraphics/ShapedWindowDemo.java
@@ -7,65 +7,24 @@ import java.awt.geom.Ellipse2D;
 import static java.awt.GraphicsDevice.WindowTranslucency.*;
 
 public class ShapedWindowDemo extends JFrame {
-    public ShapedWindowDemo() {
-        super("ShapedWindow");
-        setLayout(new GridBagLayout());
-
-        // It is best practice to set the window's shape in
-        // the componentResized method.  Then, if the window
-        // changes size, the shape will be correctly recalculated.
-        addComponentListener(new ComponentAdapter() {
-            // Give the window an elliptical shape.
-            // If the window is resized, the shape is recalculated here.
-            @Override
-            public void componentResized(ComponentEvent e) {
-                setShape(new Ellipse2D.Double(0,0,getWidth(),getHeight()));
-            }
-        });
-
+    private int displayWidth = 300;
+    private int displayHeight = 200;
+    public ShapedWindowDemo(){
+        super();
+        this.setSize(500, 400);
+        this.setLocation(50, 50);
         setUndecorated(true);
-        setSize(300,200);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBackground(new Color(0,0,0,0)); // this is the critical line - that fourth 0 represents alpha (or opacity)
 
-        add(new JButton("I am a Button"));
+        setAlwaysOnTop( true );  // keeps it in the foreground so you don't click away from it - note that clicks on the transparent part DO pass through to the desktop, at least on Lion
+
+        JLabel testLabel = new JLabel("Floating text hah");
+        this.add(testLabel);
+
     }
-
-    public static void main(String[] args) {
-        // Determine what the GraphicsDevice can support.
-        GraphicsEnvironment ge =
-                GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice gd = ge.getDefaultScreenDevice();
-        final boolean isTranslucencySupported =
-                gd.isWindowTranslucencySupported(TRANSLUCENT);
-
-        //If shaped windows aren't supported, exit.
-        if (!gd.isWindowTranslucencySupported(PERPIXEL_TRANSPARENT)) {
-            System.err.println("Shaped windows are not supported");
-            System.exit(0);
-        }
-
-        //If translucent windows aren't supported,
-        //create an opaque window.
-        if (!isTranslucencySupported) {
-            System.out.println(
-                    "Translucency is not supported, creating an opaque window");
-        }
-
-        // Create the GUI on the event-dispatching thread
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                ShapedWindowDemo sw = new ShapedWindowDemo();
-
-                // Set the window to 70% translucency, if supported.
-                if (isTranslucencySupported) {
-                    sw.setOpacity(0.7f);
-                }
-
-                // Display the window.
-                sw.setVisible(true);
-            }
-        });
+    public static void main(String[] args){
+        System.out.println("Sup");
+        ShapedWindowDemo t = new ShapedWindowDemo();
+        t.setVisible(true);
     }
 }
