f1f34d9aaa1549477aff1e6a4a6dcbd5f47ccb7a
QuantumDeity <greasyRooster1@gmail.com>
2023-09-16 21:24:46 -0700

    Made widget draggable and redid the packages

--- src/main/java/com/WindowlessGraphics/WindowlessGraphicsTest.java
+++ src/main/java/com/WindowlessGraphics/WindowlessGraphicsTest.java
@@ -1,17 +1,15 @@
-package main.java.QuantumDeity.WindowlessGraphics;
+package main.java.com.WindowlessGraphics;
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.awt.geom.Ellipse2D;
 import java.io.File;
 
-import static java.awt.GraphicsDevice.WindowTranslucency.*;
-
-public class ShapedWindowDemo extends JFrame {
+public class WindowlessGraphicsTest extends JFrame {
     private int displayWidth = 300;
     private int displayHeight = 200;
-    public ShapedWindowDemo(){
+    private int mouseX,mouseY;
+    public WindowlessGraphicsTest(){
         super();
         this.setSize(500, 400);
         this.setLocation(50, 50);
@@ -21,16 +19,31 @@ public class ShapedWindowDemo extends JFrame {
         setAlwaysOnTop( true );  // keeps it in the foreground, so you don't click away from it - note that clicks on the transparent part DO pass through to the desktop, at least on Lion
 
         String filePath = new File("").getAbsolutePath();
-        System.out.println(filePath);
         filePath = filePath.concat("\\src\\main\\java\\QuantumDeity\\WindowlessGraphics\\assets\\Axolotl_Base.png");
-        System.out.println(filePath);
+
+
+
+        addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                setLocation(getX() + e.getX() - mouseX, getY() + e.getY() - mouseY);
+            }
+        });
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                mouseX = e.getX();
+                mouseY = e.getY();
+            }
+        });
+
         JLabel imageLabel = new JLabel(new ImageIcon(filePath));
         this.add(imageLabel);
 
     }
     public static void main(String[] args){
         System.out.println("Sup");
-        ShapedWindowDemo t = new ShapedWindowDemo();
+        WindowlessGraphicsTest t = new WindowlessGraphicsTest();
         t.setVisible(true);
     }
 }
